3f626ef3a56926c3d1082960caa9f8c4e5dc032c
qianiaoo <qianiaoo@gmail.com>
2022-05-29 12:49:59 +0900

    over learn

---
+++ postcss.config.js
@@ -0,0 +1,30 @@
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
+// tailwind.config.js
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    // For the best performance and to avoid false positives,
+    // be as specific as possible with your content configuration.
+  ],
+  "plugins": [
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        "autoprefixer": {
+          "flexbox": "no-2009"
+        },
+        "stage": 3,
+        "features": {
+          "custom-properties": false
+        }
+      }
+    ]
+  ]
+};
